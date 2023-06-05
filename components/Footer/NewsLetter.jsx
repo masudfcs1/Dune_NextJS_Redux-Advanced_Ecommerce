a333e4d5f5654dd7e439e9ce0d36c5fe48092bc5
masudfcs1 <masudfcs1@gmail.com>
2023-06-05 18:46:01 +0600

    Dune

--- components/Footer/NewsLetter.jsx
+++ components/Footer/NewsLetter.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "./styles.module.scss";
+import Link from "next/link";
 const NewsLetter = () => {
-  return <div>Signup for our newsletter</div>;
+  return (
+    <div className={styles.footer__newsletter}>
+      <h3>Signup for our newsletter</h3>
+      <div className={styles.footer__flex}>
+        <input type="text" placeholder="Enter your email" />
+        <button className={styles.btn_primary}>Subscribe</button>
+      </div>
+      <p>
+        By clicking the SUBSCRIBE button, you are agreeing to{" "}
+        <Link href="">
+          <span>our Privacy & Cookie Policy</span>
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default NewsLetter;
--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./styles.module.scss";
 import Links from "./Links";
 import Socials from "./Socials";
+import NewsLetter from "./NewsLetter";
+import Payment from "./Payment";
 
 const index = () => {
   return (
@@ -9,6 +11,8 @@ const index = () => {
       <div className={styles.footer__container}>
         <Links />
         <Socials />
+        <NewsLetter />
+        <Payment />
       </div>
     </footer>
   );
