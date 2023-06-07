bb5b6df8c3ac84aa654cc715f6e941298bb6f302
masudfcs1 <masudfcs1@gmail.com>
2023-06-07 09:07:05 +0600

    styles

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,7 +1,26 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import React from "react";
+import styles from "@/styles/signin.module.scss";
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 const signin = () => {
-  return <div>signin signin</div>;
+  return (
+    <div>
+      {" "}
+      <Header />
+      <div className={styles.login}>
+        <div className={styles.login__container}>
+          <div className={styles.login__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer country={"Ban"} />{" "}
+    </div>
+  );
 };
 
 export default signin;
