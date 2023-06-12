f20ae4218b2936b0610d30053487ef31cfbc88ba
masudfcs1 <masudfcs1@gmail.com>
2023-06-12 20:28:58 +0600

    Dune

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,9 +1,15 @@
 import nc from "next-connect";
+import db from "../../../utils/db";
 
 const handler = nc();
 
 handler.post(async (req, res) => {
-  res.send("Welcome Sign up api");
+  try {
+    await db.connectDb();
+    console.log(req.body);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default handler;
