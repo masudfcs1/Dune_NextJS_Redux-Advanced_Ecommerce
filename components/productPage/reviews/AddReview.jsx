eb14e1d5f5dbc09758f15d48b23874c830610f69
masudfcs1 <masudfcs1@gmail.com>
2023-06-20 04:43:30 +0600

    Dune

--- components/productPage/reviews/AddReview.jsx
+++ components/productPage/reviews/AddReview.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Select from "./Select";
 
 export default function AddReview({ product }) {
-  return <div className={styles.reviews__add}></div>;
+  const [size, setSize] = useState("");
+  return (
+    <div className={styles.reviews__add}>
+      <div className={styles.reviews__add_wrap}>
+        <div className={styles.flex} style={{ gap: "10px" }}>
+          <Select property={size} text="Size" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/productPage/reviews/Select.jsx
+++ components/productPage/reviews/Select.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
-export default function Select() {
-  return <div>Select</div>;
+import { IoArrowDown } from "react-icons/io5";
+import { useState } from "react";
+
+export default function Select({ property, text, data, handleChange }) {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className={styles.select}>
+      <div className={styles.select__header}></div>
+    </div>
+  );
 }
